b7a34572353a599e0d924a31c5589501aaa4bbec
BinqiangLiu <134038370+BinqiangLiu@users.noreply.github.com>
2023-08-17 15:01:45 +0800

    Update app.py

--- app.py
+++ app.py
@@ -23,8 +23,7 @@ HUGGINGFACEHUB_API_TOKEN = os.getenv("HUGGINGFACEHUB_API_TOKEN")
 #huggingface.api_token = os.environ.get("HUGGINGFACEHUB_API_TOKEN")
 os.environ["HUGGINGFACEHUB_API_TOKEN"] = HUGGINGFACEHUB_API_TOKEN
 
-def get_pdf_text(pdf_files):
-    
+def get_pdf_text(pdf_files):    
     text = ""
     for pdf_file in pdf_files:
         reader = PdfReader(pdf_file)
@@ -32,59 +31,40 @@ def get_pdf_text(pdf_files):
             text += page.extract_text()
     return text
 
-def get_chunk_text(text):
-    
+def get_chunk_text(text):    
     text_splitter = CharacterTextSplitter(
     separator = "\n",
     chunk_size = 1000,
     chunk_overlap = 200,
     length_function = len
     )
-
     chunks = text_splitter.split_text(text)
-
     return chunks
 
-
-def get_vector_store(text_chunks):
-    
-    # For OpenAI Embeddings
-    
-    embeddings = OpenAIEmbeddings()
-    
+def get_vector_store(text_chunks):    
+    # For OpenAI Embeddings    
+    embeddings = OpenAIEmbeddings()    
     # For Huggingface Embeddings
-
     #embeddings = HuggingFaceInstructEmbeddings(model_name = "hkunlp/instructor-xl")
-
-    vectorstore = FAISS.from_texts(texts = text_chunks, embedding = embeddings)
-    
+    vectorstore = FAISS.from_texts(texts = text_chunks, embedding = embeddings)    
     return vectorstore
 
-def get_conversation_chain(vector_store):
-    
+def get_conversation_chain(vector_store):    
     # OpenAI Model
-
     llm = ChatOpenAI()
-
     # HuggingFace Model
-
     #llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
-
     memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
-
     conversation_chain = ConversationalRetrievalChain.from_llm(
         llm = llm,
         retriever = vector_store.as_retriever(),
         memory = memory
     )
-
     return conversation_chain
 
 def handle_user_input(question):
-
     response = st.session_state.conversation({'question':question})
     st.session_state.chat_history = response['chat_history']
-
     for i, message in enumerate(st.session_state.chat_history):
         if i % 2 == 0:
             st.write(user_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
@@ -103,33 +83,27 @@ def main():
     
     st.header('Chat with Your own PDFs :books:')
     question = st.text_input("Ask anything to your PDF: ")
-
     if question !="":
         handle_user_input(question)        
-    else:
-        st.write("Enter your question first."),
-        #st.stop()
+#    else:
+#        st.write("Enter your question first."),
+#        st.stop()
 
     with st.sidebar:
         st.subheader("Upload your Documents Here: ")
         pdf_files = st.file_uploader("Choose your PDF Files and Press OK", type=['pdf'], accept_multiple_files=True)
-
         if st.button("OK"):
             with st.spinner("Processing your PDFs..."):
-
                 # Get PDF Text
                 raw_text = get_pdf_text(pdf_files)
-
                 # Get Text Chunks
-                text_chunks = get_chunk_text(raw_text)
-                
+                text_chunks = get_chunk_text(raw_text)                
                 # Create Vector Store                
                 vector_store = get_vector_store(text_chunks)
                 st.write("DONE")
 
                 # Create conversation chain
                 st.session_state.conversation =  get_conversation_chain(vector_store)
-
-
+                
 if __name__ == '__main__':
     main()
