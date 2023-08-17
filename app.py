bfc9327f1bbbb06d08fa7699068582b661af8e5d
BinqiangLiu <134038370+BinqiangLiu@users.noreply.github.com>
2023-08-17 16:44:48 +0800

    Update app.py

--- app.py
+++ app.py
@@ -71,7 +71,7 @@ def get_conversation_chain(vector_store):
 #        else:
 #            st.write(bot_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
 
-def handle_user_input(question):
+def handle_user_input(question):   
     if question !="":
         try:
             response = st.session_state.conversation({'question':question})
@@ -81,6 +81,11 @@ def handle_user_input(question):
                     st.write(user_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
                 else:
                     st.write(bot_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
+            # 在循环结束后显示最后一条机器人的回复
+            if len(st.session_state.chat_history) % 2 == 1:
+                last_bot_message = st.session_state.chat_history[-1].content
+                st.write(bot_template.replace("{{MSG}}", last_bot_message), unsafe_allow_html=True)
+        
         except Exception as e:
             # Handle the error, e.g., print an error message or return a default text
             st.write("Documents not uploaded. Please upload your docs first and then enter your question.")     
